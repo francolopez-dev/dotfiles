d4dfc94cf2d3af9e426d4376b323c533c21668c7
Franco <jfranco8@outlook.com>
2026-02-24 11:59:24 -0500

    Initial dotfiles

---
+++ stow/shell/.bashrc
@@ -0,0 +1,3 @@
+# Dotfiles shell config
+[ -f "$HOME/.config/shell/env.sh" ] && . "$HOME/.config/shell/env.sh"
+[ -f "$HOME/.config/shell/aliases.sh" ] && . "$HOME/.config/shell/aliases.sh"
---
+++ stow/shell/.config/shell/aliases.sh
@@ -0,0 +1,8 @@
+alias ll='ls -lah'
+alias ..='cd ..'
+alias gs='git status'
+alias gp='git pull'
+alias dps='docker ps'
+alias dcu='docker compose up -d'
+alias dcd='docker compose down'
+alias ff='fastfetch 2>/dev/null || true'

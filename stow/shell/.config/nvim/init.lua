746d5d9618836ccc1908d6429216354442bb4428
Franco <jfranco8@outlook.com>
2026-02-24 15:20:09 -0500

    remove topfolders

--- stow/shell/.config/nvim/init.lua
+++ stow/shell/.config/nvim/init.lua
@@ -1,2 +1,4 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+vim.keymap.set("n", "<leader>l", "<Cmd>bnext<CR>", { silent = true })
+vim.keymap.set("n", "<leader>h", "<Cmd>bprevious<CR>", { silent = true })
--- stow/shell/.config/shell/aliases.sh
+++ stow/shell/.config/shell/aliases.sh
@@ -1,4 +1,3 @@
-alias ll='ls -lah'
 alias ..='cd ..'
 alias gs='git status'
 alias gp='git pull'
@@ -9,3 +8,10 @@ alias ff='fastfetch 2>/dev/null || true'
 
 # system apps
 alias v='nvim'
+
+#in case i use EZA
+if command -v eza >/dev/null 2>&1; then
+  alias ll='eza -lah'
+else
+  alias ll='ls -lah'
+fi

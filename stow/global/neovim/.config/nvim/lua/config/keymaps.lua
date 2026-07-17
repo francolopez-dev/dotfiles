51f860a2c11c4d5c02803b319caf9e575524ef91
Franco <jfranco8@outlook.com>
2026-07-17 10:01:34 -0400

    nvim: add app-style keymaps

--- stow/global/neovim/.config/nvim/lua/config/keymaps.lua
+++ stow/global/neovim/.config/nvim/lua/config/keymaps.lua
@@ -5,6 +5,19 @@
 -- Easier line boundaries
 vim.keymap.set({ "n", "v" }, "-", "$", { desc = "Go to end of line" })
 
+-- App-like save and search
+vim.keymap.set({ "n", "i", "v" }, "<D-s>", "<Cmd>w<CR>", { desc = "Save file" })
+vim.keymap.set("n", "<D-f>", "/", { desc = "Search in file" })
+vim.keymap.set("i", "<D-f>", "<Esc>/", { desc = "Search in file" })
+
+-- App-like comment toggles using LazyVim's default comment mappings
+vim.keymap.set("n", "<C-/>", "gcc", { desc = "Toggle comment", remap = true })
+vim.keymap.set("v", "<C-/>", "gc", { desc = "Toggle comment", remap = true })
+vim.keymap.set("n", "<C-_>", "gcc", { desc = "Toggle comment", remap = true })
+vim.keymap.set("v", "<C-_>", "gc", { desc = "Toggle comment", remap = true })
+vim.keymap.set("n", "<D-/>", "gcc", { desc = "Toggle comment", remap = true })
+vim.keymap.set("v", "<D-/>", "gc", { desc = "Toggle comment", remap = true })
+
 -- Move lines up/down
 vim.keymap.set("n", "<A-Down>", ":m .+1<CR>==", { desc = "Move line down" })
 vim.keymap.set("n", "<A-Up>", ":m .-2<CR>==", { desc = "Move line up" })
@@ -12,6 +25,12 @@ vim.keymap.set("i", "<A-Down>", "<Esc>:m .+1<CR>==gi", { desc = "Move line down"
 vim.keymap.set("i", "<A-Up>", "<Esc>:m .-2<CR>==gi", { desc = "Move line up" })
 vim.keymap.set("v", "<A-Down>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
 vim.keymap.set("v", "<A-Up>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+
+-- Duplicate lines/selections up/down
+vim.keymap.set("n", "<A-S-Down>", "yyp", { desc = "Duplicate line down" })
+vim.keymap.set("n", "<A-S-Up>", "yyP", { desc = "Duplicate line up" })
+vim.keymap.set("v", "<A-S-Down>", ":t'><CR>gv", { desc = "Duplicate selection down" })
+vim.keymap.set("v", "<A-S-Up>", ":t'<-1<CR>gv", { desc = "Duplicate selection up" })
 
 -- Reliable Alt+h/j/k/l alternatives for terminals that don't pass Alt+arrows cleanly
 vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
